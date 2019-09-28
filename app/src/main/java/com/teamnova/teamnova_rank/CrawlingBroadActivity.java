ee0337ee7181b562c902abd7c6442514e53ba628
stevenkim18 <11882@naver.com>
2019-09-28 13:19:15 +0900

    자바, 안드, PHP, 응용1, 응용2 작품 크롤링 완료

--- app/src/main/java/com/teamnova/teamnova_rank/CrawlingBroadActivity.java
+++ app/src/main/java/com/teamnova/teamnova_rank/CrawlingBroadActivity.java
@@ -14,14 +14,15 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CrawlingBroadActivity extends AppCompatActivity {
 
     // 디버깅을 위한 TAG
     private final static String TAG = "크롤링액티비티";
 
-    // 크롤링할 url 주보
-    private final static String URL = "https://cafe.naver.com/ArticleList.nhn?search.clubid=29412673&search.menuid=22&search.boardtype=C&search.totalCount=151&search.page=1";
+    // 크롤링 할 URL 주소를 담는 리스트
+    private ArrayList<String> urls;
 
     // 크롤링 시작 버튼
     Button start_crawling_btn;
@@ -32,6 +33,9 @@ public class CrawlingBroadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_crawling_broad);
 
         start_crawling_btn = findViewById(R.id.start_crawling_btn);
+
+        // url 주소 리스트 만들기
+        makeUrlList();
 
         // 버튼 클릭 시
         start_crawling_btn.setOnClickListener(new View.OnClickListener() {
@@ -60,70 +64,46 @@ public class CrawlingBroadActivity extends AppCompatActivity {
         protected Void doInBackground(Void... voids) {
 
             try {
-                // url 주소로 html 파일 가지고 오기
-                Document document = Jsoup.connect(URL).get();
 
-                // 팀노바 오픈 카페 작품 페이지에서 게시글이 있는 부분만 크롤링 함.
-                Elements broad_list = document.select("#main-area > ul.article-movie-sub > li");
+                for(int url_num = 0; url_num < urls.size(); url_num++){
 
-                Log.v("크롤링", "크롤링한 개시글 갯수 = " + broad_list.size());
+                    Log.v(TAG, "==============================================================================================");
 
-                for (int i = 0; i < broad_list.size(); i++){
+                    int page_num = 0;
 
-                    // 게시글 제목
-                    Log.v(TAG, "제목: " + broad_list.get(i).getElementsByClass("inner").text());
-                    // 작성자
-                    Log.v(TAG, "작성자: " + broad_list.get(i).getElementsByClass("m-tcol-c").text());
-                    // 게시 날짜
-                    Log.v(TAG, "날짜: " + broad_list.get(i).getElementsByClass("date").text());
-                    // 조회수
-                    // 조회수와 댓글수의 tag class 가 모두 "num"으로 되어 있기 때문에 get(0)을 해서 조회수 값만 가지고 옴.
-                    // 조회수 값을 크롤링 할 때 숫자 앞에 "조회" 텍스트가 같이 와서 띄어쓰기로 쪼갠 후 숫자 값만 가지고 옴.
-                    Log.v(TAG, "조회수: " + broad_list.get(i).getElementsByClass("num").get(0).text().split(" ")[1]);
-                    // 댓글 수
-                    // 댓글수 값을 크롤링 할 때 숫자 앞에 "댓글" 텍스트가 같이 와서 띄어쓰기로 쪼갠 후 숫자 값만 가지고 옴.
-                    Log.v(TAG, "댓글수: " + broad_list.get(i).getElementsByClass("comment_area").text().split(" ")[1]);
-                    // 좋아요 수
-                    Log.v(TAG, "좋아요수: " + broad_list.get(i).getElementsByClass("u_cnt num-recomm").text());
+                    while (true){
 
+                        //페이지 1증가
+                        page_num++;
 
+                        // 각 작품의 페이지 url 과 페이지 숫자를 합침.
+                        //ex) "https://cafe.naver.com/ArticleList.nhn?search.clubid=29412673&search.menuid=22&search.boardtype=C&search.totalCount=151&search.page=10"
+                        String crawlingUrl = urls.get(url_num) + page_num;
 
-                    // 게시물 Url과 썸네일 Url을 가지고 오기기 a태그 부분만 html 파일을 불러옴.
-                    /*
-                    <a href="/ArticleRead.nhn?clubid=29412673&amp;menuid=22&amp;boardtype=C&amp;page=1&amp;articleid=1136&amp;referrerAllArticles=false">
-						<img src="https://phinf.pstatic.net/image.nmv/cafe_2019_09_23_1220/834d0881-dddf-11e9-9adc-48df37269fd0_01.jpg?type=f100x100" width="120" height="120" alt="썸네일 이미지">
-                            <em class="ico_newcafe ico_play">
-                            동영상
-                            </em>
-					</a>
-					*/
-                    Document imageTag = Jsoup.parse(broad_list.get(i).getElementsByClass("movie-img").html());
+                        Log.v(TAG, "URL = " + crawlingUrl);
 
-                    // "a"태그에 접근
-                    Elements aTag = imageTag.select("a");
-                    // "img"태그에 접근
-                    Elements imgTag = imageTag.select("a > img");
+                        // url 주소로 html 파일 가지고 오기
+                        Document document = Jsoup.connect(crawlingUrl).get();
 
-                    // "a"태그의 "href" 값을 저장
-                    String postUrl = "https://cafe.naver.com/teamnovaopen" + aTag.get(0).attr("href");
-                    // "img"태그의 "src" 값을 저장
-                    String imgUrl = imgTag.get(0).attr("src");
+                        // 해당 페이지 게시물에 없을 때는 크롤링 멈춤.
+                        if(document.select("#main-area > ul.article-movie-sub > li").size() == 0){
+                            break;
+                        }
 
-                    // 게시글 보기 링크 url
-                    Log.v(TAG, "게시글 url: " + postUrl);
-                    // 동영상 썸네일 url
-                    Log.v(TAG, "썸네일 url: " + imgUrl);
+                        // 크롤링 시작
+                        getDataFromWebPage(document);
 
+                    }
+
+                    Log.v(TAG, "크롤링한 페이지 수 = " + (page_num - 1));
 
                 }
 
-
-
+                Log.v(TAG, "==============================================================================================");
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
 
             return null;
         }
@@ -134,6 +114,83 @@ public class CrawlingBroadActivity extends AppCompatActivity {
             super.onPostExecute(aVoid);
         }
 
+    }
+
+    //웹페이지에서 크롤링하기
+    private void getDataFromWebPage(Document document){
+        // 팀노바 오픈 카페 작품 페이지에서 게시글이 있는 부분만 크롤링 함.
+        Elements broad_list = document.select("#main-area > ul.article-movie-sub > li");
+
+        Log.v("크롤링", "크롤링한 개시글 갯수 = " + broad_list.size());
+
+        for (int i = 0; i < broad_list.size(); i++){
+
+            Log.v(TAG, "------------------------------------------------------------ " + (i + 1) + "번째 게시글 -----------------------------------------------------------------");
+
+            // 게시글 제목
+            Log.v(TAG, "제목: " + broad_list.get(i).getElementsByClass("inner").text());
+
+            // 작성자
+            Log.v(TAG, "작성자: " + broad_list.get(i).getElementsByClass("m-tcol-c").text());
+
+            // 게시 날짜
+            Log.v(TAG, "날짜: " + broad_list.get(i).getElementsByClass("date").text());
+
+            // 조회수
+            // 조회수와 댓글수의 tag class 가 모두 "num"으로 되어 있기 때문에 get(0)을 해서 조회수 값만 가지고 옴.
+            // 조회수 값을 크롤링 할 때 숫자 앞에 "조회" 텍스트가 같이 와서 띄어쓰기로 쪼갠 후 숫자 값만 가지고 옴.
+            Log.v(TAG, "조회수: " + broad_list.get(i).getElementsByClass("num").get(0).text().split(" ")[1]);
+
+            // 댓글 수
+            // 댓글수 값을 크롤링 할 때 숫자 앞에 "댓글" 텍스트가 같이 와서 띄어쓰기로 쪼갠 후 숫자 값만 가지고 옴.
+            Log.v(TAG, "댓글수: " + broad_list.get(i).getElementsByClass("comment_area").text().split(" ")[1]);
+
+            // 좋아요 수
+            Log.v(TAG, "좋아요수: " + broad_list.get(i).getElementsByClass("u_cnt num-recomm").text());
+
+            //게시글 url
+            String post_url = "https://cafe.naver.com/teamnovaopen" + broad_list.get(i).getElementsByClass("tit").attr("href");
+            Log.v(TAG, "게시글 태그: " + post_url);
+
+            Document image_tag = Jsoup.parse(broad_list.get(i).getElementsByClass("movie-img").html());
+
+            Log.v(TAG, "이미지 태그 = " + image_tag.text());
+
+            if(image_tag.text().equals("동영상")){
+                // "img"태그에 접근
+                Elements img_tag = image_tag.select("a > img");
+
+                // "img"태그의 "src" 값을 저장
+                String img_url = img_tag.get(0).attr("src");
+
+                // 동영상 썸네일 url
+                Log.v(TAG, "썸네일 url: " + img_url);
+
+            }
+            else {
+                // "img"태그의 "src" 값을 저장
+                String img_url = null;
+
+                // 동영상 썸네일 url
+                Log.v(TAG, "썸네일 url: " + img_url);
+            }
+
+
+        }
+
+    }
+
+    // 크롤링 할 주소들을 리스트에 넣기
+    private void makeUrlList(){
+        // URL 주소 리스트 생성
+        urls = new ArrayList<>();
+
+        //주소 추가
+        urls.add("https://cafe.naver.com/ArticleList.nhn?search.clubid=29412673&search.menuid=22&search.boardtype=C&search.totalCount=151&search.page=");   //자바
+        urls.add("https://cafe.naver.com/ArticleList.nhn?search.clubid=29412673&search.menuid=26&search.boardtype=C&search.totalCount=101&search.page=");   //안드로이드
+        urls.add("https://cafe.naver.com/ArticleList.nhn?search.clubid=29412673&search.menuid=27&search.boardtype=C&search.totalCount=81&search.page=");    //PHP
+        urls.add("https://cafe.naver.com/ArticleList.nhn?search.clubid=29412673&search.menuid=23&search.boardtype=C&search.totalCount=44&search.page=");    //응용1단계
+        urls.add("https://cafe.naver.com/ArticleList.nhn?search.clubid=29412673&search.menuid=24&search.boardtype=C&search.totalCount=31&search.page=");    //응용2단계
 
     }
 }

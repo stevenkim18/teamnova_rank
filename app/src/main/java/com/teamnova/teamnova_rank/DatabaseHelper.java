af356befe797692802dc50b5c502b2f4edf47960
shimyounghyun <lhlrol1004@naver.com>
2019-09-28 09:36:15 +0900

    심영현 테스트

--- app/src/main/java/com/teamnova/teamnova_rank/DatabaseHelper.java
+++ app/src/main/java/com/teamnova/teamnova_rank/DatabaseHelper.java
@@ -13,6 +13,10 @@ import java.util.List;
  * 클래스 명 : DatabaseHelper class
  * 설명 : 크롤링한 데이터를 저장하고 사용한다.
  * extends SQLiteOpenHelper한 이유 : 앱 최초 실행시 테이블을 생성, 기존 테이블이 있는 경우 삭제후 다시 생성한다. 에러 발생을 막아준다.
+ *
+ * 규칙 (https://trello.com/b/i59vsfKE/scstnghks - 규칙카드)
+ * 테이블 명 : TABLE_NAME_테이블명
+ * 컬럼 명 : 테이블명_컬럼명
  */
 public class DatabaseHelper extends SQLiteOpenHelper implements RankDataInterface {
 
@@ -24,34 +28,33 @@ public class DatabaseHelper extends SQLiteOpenHelper implements RankDataInterfac
     private static final String TABLE_NAME_RANK = "RANK_INFO_TB";
 
     /* 데이터 타입 */
-    // https://trello.com/b/i59vsfKE/scstnghks : 데이터 모델 구조 참조
-    private static final String RANK_COLUMN_RANK_ID = "RANK_ID";
-    private static final String RANK_COLUMN_TITLE = "TITLE";
-    private static final String RANK_COLUMN_WRITER = "WRITER";
-    private static final String RANK_COLUMN_CREATE_DATE = "CREATE_DATE";
-    private static final String RANK_COLUMN_DETAIL_LINK = "DETAIL_LINK";
-    private static final String RANK_COLUMN_THUMB_PATH = "THUMB_PATH";
-    private static final String RANK_COLUMN_VIEW_COUNT = "VIEW_COUNT";
-    private static final String RANK_COLUMN_LIKE_COUNT = "LIKE_COUNT";
-    private static final String RANK_COLUMN_REPLY_COUNT = "REPLY_COUNT";
-    private static final String RANK_COLUMN_RANKING = "RANKING";
-    private static final String RANK_COLUMN_TYPE = "TYPE";
+    private static final String RANK_RANK_ID = "RANK_ID"; //식별 문자
+    private static final String RANK_TITLE = "TITLE"; //게시글 제목
+    private static final String RANK_WRITER = "WRITER"; //게시글 작성자
+    private static final String RANK_CREATE_DATE = "CREATE_DATE"; //게시글 작성일
+    private static final String RANK_DETAIL_LINK = "DETAIL_LINK"; //상세 보기 링크
+    private static final String RANK_THUMB_PATH = "THUMB_PATH"; //썸네일 url
+    private static final String RANK_VIEW_COUNT = "VIEW_COUNT"; //조회수
+    private static final String RANK_LIKE_COUNT = "LIKE_COUNT"; //좋아요 개수
+    private static final String RANK_REPLY_COUNT = "REPLY_COUNT"; //댓글 개수
+    private static final String RANK_RANKING = "RANKING"; //랭킹
+    private static final String RANK_TYPE = "TYPE"; //작품 단계(기초, ..., 응용1, 응용2)
 
 
     /* 테이블 생성 쿼리 */
     private static final String CREATE_RANK_TABLE =
             "CREATE TABLE " + TABLE_NAME_RANK + "(" +
-                    RANK_COLUMN_RANK_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                    RANK_COLUMN_TITLE + " TEXT, " +
-                    RANK_COLUMN_WRITER + " TEXT, " +
-                    RANK_COLUMN_CREATE_DATE + " TEXT, " +
-                    RANK_COLUMN_DETAIL_LINK + " TEXT, " +
-                    RANK_COLUMN_THUMB_PATH + " TEXT, " +
-                    RANK_COLUMN_VIEW_COUNT + " INTEGER, " +
-                    RANK_COLUMN_LIKE_COUNT + " INTEGER, " +
-                    RANK_COLUMN_REPLY_COUNT + " INTEGER, " +
-                    RANK_COLUMN_RANKING + " INTEGER, " +
-                    RANK_COLUMN_TYPE + " INTEGER " +
+                    RANK_RANK_ID + "INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    RANK_TITLE + " TEXT, " +
+                    RANK_WRITER + " TEXT, " +
+                    RANK_CREATE_DATE + " TEXT, " +
+                    RANK_DETAIL_LINK + " TEXT, " +
+                    RANK_THUMB_PATH + " TEXT, " +
+                    RANK_VIEW_COUNT + " INTEGER, " +
+                    RANK_LIKE_COUNT + " INTEGER, " +
+                    RANK_REPLY_COUNT + " INTEGER, " +
+                    RANK_RANKING + " INTEGER, " +
+                    RANK_TYPE + " INTEGER " +
                     ")";
 
     public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {

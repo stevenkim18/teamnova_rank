834ce093f31aea85ee3e5eeb6b3828ff9d56bfa8
soohwan <scstnghks@naver.com>
2019-09-30 19:19:29 +0900

    리사이클러뷰 스크롤러 클래스   + 상세설명 다이어로그 클래스

---
+++ app/src/main/java/com/teamnova/teamnova_rank/RankDescriptionActivity.java
@@ -0,0 +1,53 @@
+package com.teamnova.teamnova_rank;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RankDescriptionActivity extends Dialog {
+    private  TextView rankTitle,rankPoint,noramalRanking,rankView,rankName,rankLike,rankReply;
+
+    private String rankWriterName;
+    private RankData rankData;
+
+    public void setRankData(RankData rankData) {
+        this.rankData = rankData;
+    }
+
+    private Activity RankDescriptionActivity;
+    public RankDescriptionActivity(@NonNull Context context) {
+        super(context);
+        Activity RankDescriptionActivity = (context instanceof Activity) ? (Activity)context:null;
+        if(RankDescriptionActivity != null){
+            this.RankDescriptionActivity =RankDescriptionActivity;
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rank_description);
+
+
+        rankName = findViewById(R.id.rank_name);
+        rankLike = findViewById(R.id.rank_like);
+        rankReply = findViewById(R.id.rank_reply);
+        rankView = findViewById(R.id.rank_view);
+
+
+//        rankData = getIntent().getParcelableExtra("rankData");
+//        rankWriterName = getIntent().getExtras().get("작성자명").toString();
+
+//        Toast.makeText(this,"작성자면"+rankData.getRankWriter()+"댓글수"+rankData.getReplyCount()+"좋아요수"+rankData.getLikeCount()+"조회수"+rankData.getViewCount(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(RankDescriptionActivity,"작성자면"+rankData.getRankWriter(), Toast.LENGTH_SHORT).show();
+    }
+}

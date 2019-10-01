0612decd0ff99f2fac168e412d28ea96142ebb0a
shimyounghyun <lhlrol1004@naver.com>
2019-10-01 11:47:45 +0900

    크롤러 수정

--- app/src/main/java/com/teamnova/teamnova_rank/JsoupDocumentCrawler.java
+++ app/src/main/java/com/teamnova/teamnova_rank/JsoupDocumentCrawler.java
@@ -7,79 +7,20 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
-
-public class JsoupAsyncCrawler extends AsyncTask<Integer, Void, Integer> {
-
-    public JsoupAsyncListener jsoupAsyncListener;
+public class JsoupDocumentCrawler extends AsyncTask<Void, Void, Void > {
+    public Document document;
+    public int RANK_TYPE;
     public DatabaseHelper databaseHelper;
 
-    public interface JsoupAsyncListener{
-        void onPostExecute(Integer integer);
-    }
-
-    public void setJsoupAsyncListener(JsoupAsyncListener jsoupAsyncListener) {
-        this.jsoupAsyncListener = jsoupAsyncListener;
-    }
-
-    public void setDatabaseHelper(DatabaseHelper databaseHelper) {
+    public JsoupDocumentCrawler(Document document, int RANK_TYPE, DatabaseHelper databaseHelper) {
+        this.document = document;
+        this.RANK_TYPE = RANK_TYPE;
         this.databaseHelper = databaseHelper;
     }
 
-    // AsyncTask 실행
-    @Override
-    protected Integer doInBackground(Integer... voids) {
-        final int RANK_TYPE = voids[0];
-        try {
-
-            Log.d("크롤링 시작 URL 번호 >", RANK_TYPE+"");
-
-            // 저장된 작품 단계별 삭제
-            databaseHelper.deleteRankData(RANK_TYPE);
-
-            int page_num = 0;
-
-            while (true){
-
-                //페이지 1증가
-                page_num++;
-
-                // 각 작품의 페이지 url 과 페이지 숫자를 합침.
-                //ex) "https://cafe.naver.com/ArticleList.nhn?search.clubid=29412673&search.menuid=22&search.boardtype=C&search.totalCount=151&search.page=10"
-                String crawlingUrl = Constant.CAFE_TEAMNOVA_URL_LIST.get(RANK_TYPE) + page_num;
-
-                // url 주소로 html 파일 가지고 오기
-                Document document = Jsoup.connect(crawlingUrl).get();
-
-                // 해당 페이지 게시물에 없을 때는 크롤링 멈춤.
-                if(document.select("#main-area > ul.article-movie-sub > li").size() == 0){
-                    break;
-                }
-
-                // 크롤링 시작
-                getDataFromWebPage(document, RANK_TYPE);
-
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            // 크롤링 실패 저장
-            databaseHelper.insertCrawlScheme(RANK_TYPE, false);
-        }
-        // 크롤링 성공 저장
-        databaseHelper.insertCrawlScheme(RANK_TYPE, true);
-        return RANK_TYPE;
-    }
 
     @Override
-    protected void onPostExecute(Integer integer) {
-        if(jsoupAsyncListener != null)
-            jsoupAsyncListener.onPostExecute(integer);
-        Log.d("크롤링 완료 번호 ",integer+"");
-    }
-
-    //웹페이지에서 크롤링하기
-    private void getDataFromWebPage(Document document, int RANK_TYPE){
+    protected Void doInBackground(Void... voids) {
         // 팀노바 오픈 카페 작품 페이지에서 게시글이 있는 부분만 크롤링 함.
         Elements broad_list = document.select("#main-area > ul.article-movie-sub > li");
         for (int i = 0; i < broad_list.size(); i++){
@@ -119,6 +60,12 @@ public class JsoupAsyncCrawler extends AsyncTask<Integer, Void, Integer> {
 
             databaseHelper.insertRankData(title,writer,create_date,post_url,img_url,view_count,like_count,reply_count,RANK_TYPE);
         }
+        return null;
     }
 
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+        Log.d("완료","완료");
+    }
 }

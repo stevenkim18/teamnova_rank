cf8ba2a40b00c0e6ed7674a88e1e8a334bade75e
stevenkim18 <11882@naver.com>
2019-10-01 22:20:52 +0900

    스플래쉬 화면에서 네트워크 연결 확인

--- app/src/main/java/com/teamnova/teamnova_rank/SplashActivity.java
+++ app/src/main/java/com/teamnova/teamnova_rank/SplashActivity.java
@@ -3,38 +3,36 @@ package com.teamnova.teamnova_rank;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SplashActivity extends AppCompatActivity {
 
-
+    private static final String TAG = "스플래쉬 액티비티";
+    private LottieAnimationView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        animationView = findViewById(R.id.splash_lottie);
 
-        //리시버등록
-        IntentFilter filter = new IntentFilter();
-        filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
-        registerReceiver(mReceiver, filter);
+        isNetworkConnected(getApplicationContext());
 
-
-
-    }
-
-    private class splashhandler implements Runnable{
-        public void run(){
-            startActivity(new Intent(getApplication(), MainActivity.class)); //로딩이 끝난 후, ChoiceFunction 이동
-            SplashActivity.this.finish(); // 로딩페이지 Activity stack에서 제거
-        }
     }
 
     @Override
@@ -42,46 +40,58 @@ public class SplashActivity extends AppCompatActivity {
         //초반 플래시 화면에서 넘어갈때 뒤로가기 버튼 못누르게 함
     }
 
-    //와이파이 상태변화 수신
-    BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            switch(intent.getAction()){
 
-                //네트워크 상태변화
-                case WifiManager.NETWORK_STATE_CHANGED_ACTION:
-                    NetworkInfo info = (NetworkInfo)intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-                    //네트워크 상태값 가져오기
-                    NetworkInfo.DetailedState state = info.getDetailedState();
+    private void showDialog() {
 
-                    String typename = info.getTypeName();
-                    if(state==NetworkInfo.DetailedState.CONNECTED){ //네트워크 연결
-                        Handler hd = new Handler();
-
-                        hd.postDelayed(new splashhandler(), 1000); // 1초 후에 hd handler 실행  3000ms = 3초
-                    }
-                    else if(state==NetworkInfo.DetailedState.DISCONNECTED){ //네트워크 끊음
-
-                        standardDialog();//네트워크 연결하라는 다이얼로그 띄우기
-                    }
-                    break;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("네트워크 연결 실패");
+        builder.setMessage("네트워크를 연결하고 다시 앱을 실행해 주세요");
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SplashActivity.this.finish();
             }
-        }
-    };
-
-    //리시버 해제
-    @Override
-    protected void onDestroy() {
-        unregisterReceiver(mReceiver);
-        super.onDestroy();
+        });
+        // 다이얼로그 밖에 화면 클릭 시 다이얼로그가 종료 되지 않게 하기
+        builder.setCancelable(false);
+        builder.show();
     }
 
-    //목록다이어로그에서 작품 순위 산정 기준을 선택했을 때 보여주는 다이어로그
-    public void standardDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);//다이어로그 생성해줍니다
-        builder.setTitle("WIFI 연결상태확인");//다이어로그 제목
-        builder.setMessage("WIFI를 연결해주세요");//다이어로그 내용
-        builder.show();//다이어로그 보여주기
+    private boolean isNetworkConnected(Context context){
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // 모바일 네트워크
+        boolean isMobileAvailable = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isAvailable();
+        Log.v(TAG, "" + isMobileAvailable);
+        boolean isMobileConnect = manager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).isConnectedOrConnecting();
+        Log.v(TAG, "" + isMobileConnect);
+
+        // 와이파이
+        boolean isWifiAvailable = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isAvailable();
+        Log.v(TAG, "" + isWifiAvailable);
+        boolean isWifiConnect = manager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).isConnectedOrConnecting();
+        Log.v(TAG, "" + isWifiConnect);
+
+
+        if ((isWifiAvailable && isWifiConnect) || (isMobileAvailable && isMobileConnect)){
+
+            animationView.setRepeatCount(1);
+            animationView.playAnimation();
+            animationView.addAnimatorListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                    SplashActivity.this.finish();
+                }
+            });
+
+            return true;
+        }else{
+
+            showDialog();
+
+            return false;
+        }
     }
 }
 
